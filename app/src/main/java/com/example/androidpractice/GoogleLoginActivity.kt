21c45b2b4c35cba7d35dabbe5404804da28f13cc
Mykola Demianko <mykd@protonmail.com>
2021-03-04 16:53:48 +0200

    Add GoogleLoginActivity

---
+++ app/src/main/java/com/example/androidpractice/GoogleLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.androidpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoogleLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_login)
+    }
+}
