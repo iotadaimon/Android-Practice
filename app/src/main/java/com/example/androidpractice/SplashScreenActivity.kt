31b35fb982d4b1bd4207216cfdad29c14aa1d4c9
Mykola Demianko <mykd@protonmail.com>
2021-03-04 17:23:32 +0200

    Add logic to SplashScreenActivity

--- app/src/main/java/com/example/androidpractice/SplashScreenActivity.kt
+++ app/src/main/java/com/example/androidpractice/SplashScreenActivity.kt
@@ -1,11 +1,23 @@
 package com.example.androidpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN    // Set to fullscreen
+
+        // Wait for something to be done and launch MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
