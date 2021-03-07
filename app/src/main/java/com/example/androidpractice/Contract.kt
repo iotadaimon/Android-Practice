09c9b369ca15d6c87d07d46a4f0f8c45b2fff22e
Mykola Demianko <mykd@protonmail.com>
2021-03-07 17:59:38 +0200

    Add default method arguments to getPopularMovies

--- app/src/main/java/com/example/androidpractice/Contract.kt
+++ app/src/main/java/com/example/androidpractice/Contract.kt
@@ -5,7 +5,12 @@ import com.example.androidpractice.model.entity.Movie
 interface Contract {
 
     interface Model {
-        fun getPopularMovies(apiKey: String, language: String = "en-US", page: Int = 1, region: String = ""): List<Movie>
+        fun getPopularMovies(
+                apiKey: String,
+                language: String = TMDBService.DEFAULT_LANGUAGE_ARGUMENT,
+                page: Int = TMDBService.DEFAULT_PAGE_ARGUMENT,
+                region: String = TMDBService.DEFAULT_REGION_ARGUMENT
+        ): List<Movie>
     }
 
     interface View {
--- app/src/main/java/com/example/androidpractice/TMDBService.kt
+++ app/src/main/java/com/example/androidpractice/TMDBService.kt
@@ -8,12 +8,18 @@ import retrofit2.http.Query
 // TODO - Declare overloaded methods for Java interoperability
 interface TMDBService {
 
+    companion object {
+        const val DEFAULT_LANGUAGE_ARGUMENT: String = "en-US"
+        const val DEFAULT_PAGE_ARGUMENT: Int = 1
+        const val DEFAULT_REGION_ARGUMENT: String = ""
+    }
+
     @GET("popular")
     fun getPopularMovies(
         @Query("api_key") apiKey: String,
-        @Query("language") language: String,
-        @Query("page") page: Int,
-        @Query("region") region: String
+        @Query("language") language: String = DEFAULT_LANGUAGE_ARGUMENT,
+        @Query("page") page: Int = DEFAULT_PAGE_ARGUMENT,
+        @Query("region") region: String = DEFAULT_REGION_ARGUMENT
     ): Call<TMDBResponse>
 
 }
