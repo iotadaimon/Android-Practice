719ca2bc4ef51ec18e0c639832f34ff1413726df
Mykola Demianko <mykd@protonmail.com>
2021-03-08 11:50:19 +0200

    Add LikedMoviesFragment, move all fragments to a separate package

--- app/src/main/java/com/example/androidpractice/fragment/LikedMoviesFragment.kt
+++ app/src/main/java/com/example/androidpractice/fragment/LikedMoviesFragment.kt
@@ -1,12 +1,13 @@
-package com.example.androidpractice
+package com.example.androidpractice.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.androidpractice.R
 
-class AllMoviesFragment : Fragment() {
+class LikedMoviesFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
