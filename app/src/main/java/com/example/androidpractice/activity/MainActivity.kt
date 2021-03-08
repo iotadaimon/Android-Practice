c9eae3e011126929e56167b57b0f1eee01a89a6a
Mykola Demianko <mykd@protonmail.com>
2021-03-08 11:31:49 +0200

    Add DrawerLayout and NavigationView to MainActivity

--- app/src/main/java/com/example/androidpractice/activity/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/activity/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.androidpractice.activity
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.androidpractice.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.my_drawer_layout)
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return if (actionBarDrawerToggle.onOptionsItemSelected(item))
+            true
+        else
+            super.onOptionsItemSelected(item)
     }
 }
