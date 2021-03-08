aa1343175f58c084b718220dd9f2d871fe2f0116
Mykola Demianko <mykd@protonmail.com>
2021-03-08 20:10:22 +0200

    Setup NavigationView in MainActivity

--- app/src/main/java/com/example/androidpractice/activity/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/activity/MainActivity.kt
@@ -4,7 +4,13 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.androidpractice.Contract
 import com.example.androidpractice.Presenter
 import com.example.androidpractice.R
@@ -12,22 +18,34 @@ import com.example.androidpractice.model.LocalStorageModel
 import com.example.androidpractice.model.TMDBModel
 import com.example.androidpractice.model.TMDBService
 import com.example.androidpractice.model.entity.Movie
+import com.google.android.material.navigation.NavigationView
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity(), Contract.View {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
+    Contract.View {
 
     private lateinit var presenter: Contract.Presenter
 
+    private lateinit var navigationView: NavigationView
+    private lateinit var navController: NavController
+
+    private lateinit var toolbar: Toolbar
     lateinit var drawerLayout: DrawerLayout
-    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         initPresenter()
         initDrawerMenu()
+        initNavigationView()
     }
 
     private fun initPresenter() {
@@ -40,21 +58,49 @@ class MainActivity : AppCompatActivity(), Contract.View {
     }
 
     private fun initDrawerMenu() {
-        drawerLayout = findViewById(R.id.my_drawer_layout)
-        actionBarDrawerToggle =
-            ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val actionBarDrawerToggle =
+            ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.nav_open,
+                R.string.nav_close
+            )
 
         drawerLayout.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return if (actionBarDrawerToggle.onOptionsItemSelected(item))
-            true
-        else
-            super.onOptionsItemSelected(item)
+    private fun initNavigationView() {
+        navigationView = findViewById(R.id.navigation_view)
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(
+            this,
+            R.id.nav_host_fragment
+        ).navigateUp() || super.onSupportNavigateUp()
+    }
+
+    // TODO
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        println("test")
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
     override fun showAllMovies(movies: List<Movie>) {
@@ -64,4 +110,5 @@ class MainActivity : AppCompatActivity(), Contract.View {
     override fun showLikedMovies(movies: List<Movie>) {
         TODO("Not yet implemented")
     }
+
 }
