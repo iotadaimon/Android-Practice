99f6e00edb70b04e5deb8f19ceb0cc7c7de14e7a
Mykola Demianko <mykd@protonmail.com>
2021-03-11 18:45:30 +0200

    Implement MainActivity::showAllMovies() and AllMoviesFragment::onCreateView()

--- app/src/main/java/com/example/androidpractice/activity/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice/activity/MainActivity.kt
@@ -3,17 +3,16 @@ package com.example.androidpractice.activity
 import android.os.Bundle
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.*
 import com.example.androidpractice.Contract
 import com.example.androidpractice.Presenter
 import com.example.androidpractice.R
+import com.example.androidpractice.fragment.AllMoviesFragment
 import com.example.androidpractice.model.LocalStorageModel
 import com.example.androidpractice.model.TMDBModel
 import com.example.androidpractice.model.TMDBService
@@ -21,31 +20,46 @@ import com.example.androidpractice.model.entity.Movie
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+// TODO - Load all movies at launch
+
 class MainActivity : AppCompatActivity(), Contract.View {
 
     private lateinit var presenter: Contract.Presenter
 
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
+
     private lateinit var appBarConfiguration: AppBarConfiguration
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initialisePresenter()
+
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout = findViewById(R.id.drawer_layout)
         val navigationView: NavigationView = findViewById(R.id.navigation_view)
-        val navController =
+        navController =
             (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
         appBarConfiguration = AppBarConfiguration(
             setOf(R.id.nav_all_movies, R.id.nav_liked_movies),
             drawerLayout
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navigationView.setupWithNavController(navController)
 
-        initialisePresenter()
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_all_movies -> presenter.presentAllMovies()
+                R.id.nav_liked_movies -> presenter.presentLikedMovies()
+            }
+            true
+        }
     }
 
     private fun initialisePresenter() {
@@ -62,8 +76,17 @@ class MainActivity : AppCompatActivity(), Contract.View {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
+
     override fun showAllMovies(movies: List<Movie>) {
-        TODO("Not yet implemented")
+        drawerLayout.closeDrawers()
+        val fragment =
+            AllMoviesFragment() // TODO - implement Parcelable for Movie.class, put into Bundles and pass to the fragment?
+        val bundle = Bundle()
+        bundle.putParcelableArray("data", movies.toTypedArray())
+        val ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.nav_host_fragment, fragment);
+        ft.commit();
+        navController.navigate(R.id.nav_all_movies, bundle)
     }
 
     override fun showLikedMovies(movies: List<Movie>) {
--- app/src/main/java/com/example/androidpractice/fragment/AllMoviesFragment.kt
+++ app/src/main/java/com/example/androidpractice/fragment/AllMoviesFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.androidpractice.R
+import com.example.androidpractice.model.entity.Movie
 
 class AllMoviesFragment : Fragment() {
 
@@ -17,6 +19,12 @@ class AllMoviesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // TODO - fix types
+        val movieList = arguments?.getParcelableArrayList<Movie>("data")
+
+        val recyclerView: RecyclerView? = view?.findViewById(R.id.all_movies_recycler_view)
+        recyclerView?.adapter = MovieAdapter(movieList!!.toList())
+
         return inflater.inflate(R.layout.fragment_all_movies, container, false)
     }
 
