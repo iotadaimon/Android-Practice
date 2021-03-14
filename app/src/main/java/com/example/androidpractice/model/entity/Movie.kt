f96108f345c4e2843cc367a693cbb44ae9e110fe
Mykola Demianko <mykd@protonmail.com>
2021-03-14 18:25:45 +0200

    Remove unneeded elements

--- app/src/main/java/com/example/androidpractice/model/entity/Movie.kt
+++ app/src/main/java/com/example/androidpractice/model/entity/Movie.kt
@@ -1,6 +1,5 @@
 package com.example.androidpractice.model.entity
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
