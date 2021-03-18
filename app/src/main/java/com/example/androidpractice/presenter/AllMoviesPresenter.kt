0354722f10dd55f9ee2e7039a4c819df0374b618
Mykola Demianko <mykd@protonmail.com>
2021-03-18 14:43:00 +0200

    Improve data retrieval from TMDB

--- app/src/main/java/com/example/androidpractice/presenter/AllMoviesPresenter.kt
+++ app/src/main/java/com/example/androidpractice/presenter/AllMoviesPresenter.kt
@@ -3,29 +3,32 @@ package com.example.androidpractice.presenter
 import com.example.androidpractice.MovieModel
 import com.example.androidpractice.MoviePresenter
 import com.example.androidpractice.MovieView
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.async
-import kotlinx.coroutines.runBlocking
-import java.io.IOException
+
 
 class AllMoviesPresenter(private val model: MovieModel, private val view: MovieView) :
     MoviePresenter {
 
     // TODO - Accept coroutine context as a variable
     override fun presentMovies() {
-        val movies = GlobalScope.async {
-            try {
-                model.getMovies()
-            } catch (ex: IOException) {
-                view.showErrorToast()
-                emptyList()
-            }
+        view.showProgressIndicator()
+
+        val handler = CoroutineExceptionHandler { _, exception ->
+            view.showErrorToast()
+            view.hideProgressIndicator()
         }
 
-        // TODO - implement a progress bar in the view
+        GlobalScope.launch(handler) {
+            launch(Dispatchers.Main) {
+                val movies = GlobalScope.async { model.getMovies() }.await()
 
-        runBlocking {
-            view.showMovies(movies.await())
+                view.hideProgressIndicator()
+                view.showMovies(movies)
+            }
         }
     }
 
