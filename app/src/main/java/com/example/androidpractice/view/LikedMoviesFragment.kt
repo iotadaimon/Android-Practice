2634c6ddea6c12e9d42d0665794b8b1f4da1ec0a
Mykola Demianko <mykd@protonmail.com>
2021-03-23 09:46:48 +0200

    Combine common fragment code into abstract MovieListFragment, merge fragment layouts into one

--- app/src/main/java/com/example/androidpractice/view/LikedMoviesFragment.kt
+++ app/src/main/java/com/example/androidpractice/view/LikedMoviesFragment.kt
@@ -1,84 +1,18 @@
 package com.example.androidpractice.view
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.androidpractice.Constants
-import com.example.androidpractice.MoviePresenter
 import com.example.androidpractice.MovieView
 import com.example.androidpractice.R
-import com.example.androidpractice.model.entity.Movie
 import com.example.androidpractice.model.local.LocalStorageModel
 import com.example.androidpractice.model.local.MovieDatabaseSingleton
 import com.example.androidpractice.presenter.LikedMoviesPresenter
-import com.google.android.material.progressindicator.LinearProgressIndicator
 
-class LikedMoviesFragment : Fragment(), MovieView {
-
-    private lateinit var presenter: MoviePresenter
-
-    private var progressIndicator: LinearProgressIndicator? = null
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var recyclerViewAdapter: MovieAdapter
-
+class LikedMoviesFragment : MovieListFragment(), MovieView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         presenter = LikedMoviesPresenter(LocalStorageModel(MovieDatabaseSingleton.movieDAO), this)
-        progressIndicator = activity?.findViewById(R.id.main_activity_progress_indicator)
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_liked_movies, container, false)
-
-        recyclerView = view.findViewById(R.id.liked_movies_recycler_view)
-        recyclerView.adapter = MovieAdapter(mutableListOf(), this)
-        recyclerViewAdapter = recyclerView.adapter as MovieAdapter
-
-        recyclerView.addOnScrollListener(
-            MovieListScrollListener(
-                Constants.MOVIE_LIST_PAGE_SIZE,
-                recyclerView.layoutManager as LinearLayoutManager,
-                presenter
-            )
-        )
-
-        return view
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        presenter.presentMovies()
-    }
-
-    override fun showMovies(movies: List<Movie>) {
-        recyclerViewAdapter.movieList = movies
-        recyclerViewAdapter.notifyDataSetChanged()
-    }
-
-    override fun showMovieDetails(movie: Movie) {
-        val intent = Intent(context, MovieDetailsActivity::class.java)
-        intent.putExtra(MovieDetailsActivity.DATA_MOVIE, movie)
-        startActivity(intent)
-    }
-
-    override fun showProgressIndicator() {
-        progressIndicator?.show()
-    }
-
-
-    override fun hideProgressIndicator() {
-        progressIndicator?.hide()
     }
 
     override fun showErrorToast() {
---
+++ app/src/main/java/com/example/androidpractice/view/MovieListFragment.kt
@@ -0,0 +1,75 @@
+package com.example.androidpractice.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.androidpractice.*
+import com.example.androidpractice.model.entity.Movie
+import com.google.android.material.progressindicator.LinearProgressIndicator
+
+abstract class MovieListFragment : Fragment(),
+    MovieView {
+
+    protected lateinit var presenter: MoviePresenter
+
+    private var progressIndicator: LinearProgressIndicator? = null
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerViewAdapter: MovieAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        progressIndicator = activity?.findViewById(R.id.main_activity_progress_indicator)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_movie_list, container, false)
+
+        recyclerView = view.findViewById(R.id.movie_list_recycler_view)
+        recyclerView.adapter = MovieAdapter(mutableListOf(), this)
+        recyclerViewAdapter = recyclerView.adapter as MovieAdapter
+
+        recyclerView.addOnScrollListener(
+            MovieListScrollListener(
+                Constants.MOVIE_LIST_PAGE_SIZE,
+                recyclerView.layoutManager as LinearLayoutManager,
+                presenter
+            )
+        )
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        presenter.presentMovies()
+    }
+
+    override fun showMovies(movies: List<Movie>) {
+        recyclerViewAdapter.movieList = movies
+        recyclerViewAdapter.notifyDataSetChanged()
+    }
+
+    override fun showMovieDetails(movie: Movie) {
+        val intent = Intent(context, MovieDetailsActivity::class.java)
+        intent.putExtra(MovieDetailsActivity.DATA_MOVIE, movie)
+        startActivity(intent)
+    }
+
+    override fun showProgressIndicator() {
+        progressIndicator?.show()
+    }
+
+
+    override fun hideProgressIndicator() {
+        progressIndicator?.hide()
+    }
+
+}
