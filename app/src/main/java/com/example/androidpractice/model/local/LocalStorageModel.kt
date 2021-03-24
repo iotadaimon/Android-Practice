46f15041f527d0212fff24e9004102fe80ccae46
Mykola Demianko <mykd@protonmail.com>
2021-03-24 11:22:04 +0200

    Make local database return pages instead of all movies

--- app/src/main/java/com/example/androidpractice/model/local/LocalStorageModel.kt
+++ app/src/main/java/com/example/androidpractice/model/local/LocalStorageModel.kt
@@ -6,21 +6,14 @@ import com.example.androidpractice.model.entity.Movie
 
 class LocalStorageModel(private val movieDAO: MovieDAO) : MutableMovieModel {
 
-    private val moviesToServe: MutableList<Movie> = mutableListOf()
-
     override suspend fun addMovie(movie: Movie) = movieDAO.insertAll(movie)
 
     override suspend fun removeMovie(movie: Movie) = movieDAO.delete(movie)
 
     override suspend fun getMovies(pageNumber: Int): List<Movie> {
-        if (moviesToServe.isEmpty()) moviesToServe.addAll(movieDAO.getAll()) // TODO - Load only required pages
-
         val startIndex = (pageNumber - 1) * Constants.MOVIE_LIST_PAGE_SIZE
-        val endIndex = pageNumber * Constants.MOVIE_LIST_PAGE_SIZE - 1
 
-        return moviesToServe.filterIndexed { index: Int, _ ->
-            index in startIndex..endIndex
-        }
+        return movieDAO.loadAllBetweenRows(startIndex.toLong(), Constants.MOVIE_LIST_PAGE_SIZE)
     }
 
     override suspend fun getMoviesById(id: Int): List<Movie> = movieDAO.loadAllById(id)
--- app/src/main/java/com/example/androidpractice/model/local/MovieDAO.kt
+++ app/src/main/java/com/example/androidpractice/model/local/MovieDAO.kt
@@ -12,6 +12,9 @@ interface MovieDAO {
     @Query("SELECT * FROM liked_movies")
     fun getAll(): List<Movie>
 
+    @Query("SELECT * FROM liked_movies ORDER BY title LIMIT (:rowNumber) OFFSET (:offset)")
+    fun loadAllBetweenRows(offset: Long, rowNumber:Int): List<Movie>
+
     @Query("SELECT * FROM liked_movies WHERE id = (:movieId)")
     fun loadAllById(movieId: Int): List<Movie>
 
